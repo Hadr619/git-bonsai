7c54edcce37e12ea4939d85ba08b1233f3b6f685
Perry Reid <perry.reid@faulknermediagroup.com>
2019-06-07 20:04:59 -0700

    init

---
+++ clone.js
@@ -0,0 +1,12 @@
+var nodegit = require('nodegit'),
+    path = require('path')
+
+var url = "https://github.com/pazdera/scriptster.git",
+    local = "./scriptster",
+    cloneOpts = {};
+
+nodegit.Clone(url, local, cloneOpts).then(function (repo) {
+    console.log("Cloned " + path.basename(url) + " to " + repo.workdir());
+}).catch(function (err) {
+    console.log(err);
+});
